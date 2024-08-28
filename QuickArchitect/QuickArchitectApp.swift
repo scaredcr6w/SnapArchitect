f2444c49e636b5d15ed69dabcc24655c34699b51
Levente Anda <85457088+scaredcr6w@users.noreply.github.com>
2024-08-28 15:33:51 +0200

    CanvasView launches maximized

--- QuickArchitect/QuickArchitectApp.swift
+++ QuickArchitect/QuickArchitectApp.swift
@@ -12,6 +12,18 @@ struct QuickArchitectApp: App {
     var body: some Scene {
         DocumentGroup(newDocument: QuickArchitectDocument()) { file in
             CanvasView(document: file.$document)
+                .onAppear {
+                    if let window = NSApplication.shared.windows.last {
+                        maximizeWindow(window)
+                    }
+                }
+        }
+    }
+    
+    func maximizeWindow(_ window: NSWindow) {
+        if let screen = window.screen {
+            let screenRect = screen.visibleFrame
+            window.setFrame(screenRect, display: true, animate: true)
         }
     }
 }
