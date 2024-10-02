316e15d9024f5cc90173655f90a594062b98e565
Levente Anda <85457088+scaredcr6w@users.noreply.github.com>
2024-10-02 13:05:26 +0200

    added show grid option to canvas settings (not implemented)

--- SnapArchitect/Components/Settings/CanvasSettingsView.swift
+++ SnapArchitect/Components/Settings/CanvasSettingsView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct CanvasSettingsView: View {
     @AppStorage("canvasBackgorundColor") private var backgroundColor: Color = .white
+    @AppStorage("showGrid") private var showGrid: Bool = false
     @AppStorage("snapToGrid") private var snapToGrid: Bool = false
     @AppStorage("gridSize") private var gridSize: Double = 10
     
     var body: some View {
         Form {
+            Toggle("Show grid", isOn: $showGrid)
             Toggle("Snap to grid", isOn: $snapToGrid)
             Slider(value: $gridSize, in: 1...20) {
                 Text("Grid Size: \(gridSize, specifier: "%.1f") pts")
