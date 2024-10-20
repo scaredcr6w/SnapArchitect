426fb43cabffaf703038f0b04a89f73b51034b3c
Levente Anda <85457088+scaredcr6w@users.noreply.github.com>
2024-10-20 10:37:17 +0200

    moved canvasgridshape to new file

--- SnapArchitect/Components/Canvas/CanvasView.swift
+++ SnapArchitect/Components/Canvas/CanvasView.swift
@@ -145,24 +145,3 @@ struct CanvasView: View {
         }
     }
 }
-
-
-struct CanvasGridShape: Shape {
-    var gridSize: Double
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        for x in stride(from: 0, to: rect.width, by: gridSize) {
-            path.move(to: CGPoint(x: x, y: 0))
-            path.addLine(to: CGPoint(x: x, y: rect.height))
-        }
-        
-        for y in stride(from: 0, to: rect.height, by: gridSize) {
-            path.move(to: CGPoint(x: 0, y: y))
-            path.addLine(to: CGPoint(x: rect.width, y: y))
-        }
-        
-        return path
-    }
-}
